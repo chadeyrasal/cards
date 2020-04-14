ade5a0e350aeab0b6f163d0ab85701016d450bac
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-14 15:14:25 +0100

    add case tests on top of doctests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,20 @@
+# In Elixir there are two types of tests:
+#  - Usual specific case tests
+#  - Doctesting: while writing documentation, tests are created
+
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
+  end
+
+  # Not the best test because there would be a possibility that the shuffled deck is the exact same as the origimnal one
+  test "shuffling a deck randomises it" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle(deck)
+    # refute deck == Cards.shuffle(deck) => does the same thing
   end
 end
