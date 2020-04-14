b6f40266233329fbd2680e4c9373cf805797ecc2
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-14 11:08:01 +0100

    write the contains? method that uses Enum method member? and increase the complexity of the create_deck method by adding suits to values and using double list comprehension to match each value with each of the four suits

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,37 @@
+# This command defines a module
+# In Elixir, nearly all code will be written inside modules
+# A module is a collection of different methods or functions
+
+# In functions, there can be no return keyword. Elixir has an implicit return value. Whatever the last line of a function
+# is will be the return value of that function.
+
+# Elixir has a lot of standard methods that allow you to work with lists, numbers, your file system, HTTP requests...
+
+# The concept of iummutability in Elixir that everytime a data structure is changed, it is actually a copy of the original
+# data structure that is modified according to need. A data structure cannot be mutated.
+
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  # Single quotes are supported but the convention is to use double quotes.
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    # This is a list comprehension, sort of a for loop. For every element in the list do what is written in the block
+    # The comprehension is essentially a mapping function: so every single time we take an item out of our list -
+    # suits here - it is passed in the do block.
+    # Whatever is returned from the do block is placed into a brand new array.
+    # Whatever the final array is, it is the returned value from the comprehension.
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
 
-  ## Examples
+  # Arity is the number of arguments that a function accepts
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
